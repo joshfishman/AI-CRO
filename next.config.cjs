bf2803c37da30771534fb11bedbeb2bd7ff8c8cd
Joshua Fishman <joshuafishman@Mac.lan1>
2025-05-03 11:23:31 -0700

    Fix routing configuration for app directory

--- next.config.cjs
+++ next.config.cjs
@@ -10,8 +10,33 @@ const nextConfig = {
         source: '/api/:path*',
         destination: '/api/:path*',
       },
+      {
+        source: '/',
+        destination: '/',
+      },
+      {
+        source: '/admin',
+        destination: '/admin',
+      },
+      {
+        source: '/segments',
+        destination: '/segments',
+      },
+      {
+        source: '/bookmarklet',
+        destination: '/bookmarklet',
+      }
     ];
   },
+  async redirects() {
+    return [
+      {
+        source: '/index',
+        destination: '/',
+        permanent: true,
+      }
+    ];
+  }
 };
 
 module.exports = nextConfig; 
