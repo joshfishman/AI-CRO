af897458c887215955780a8cf00329765d03cb43
Joshua Fishman <joshuafishman@Mac.lan1>
2025-05-03 11:20:01 -0700

    Fix configuration files for app directory routing

--- next.config.cjs
+++ next.config.cjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: '/api/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig; 
